7207a895f27a2fecf7d4de81025258eecd45305e
madalenaRio <madalena.rio@gmail,com>
2022-05-24 16:43:53 +0200

    apis working + add DOM manipulation + paragr

--- script.js
+++ script.js
@@ -5,66 +5,60 @@ let lat;
 let cityName;
 const key = ApiKey;
 
-const form = document.querySelector("form");
+const button = document.getElementById("button")
 const input = document.getElementById("input");
 const msg = document.getElementById("msg")
 
-
-const executeForm = (e) => {
-    // get the value from input textbox
-    e.preventDefault(); 
+const executeForm = () => {
+    // get the value from input text
     cityName = input.value;
+    console.log(cityName)
     // get coordinates from input
     getCoordinates(cityName);
-    console.log(getCoordinates + "inside executeform");
     //getWeather();
-    console.log(getWeather() + "inside executeform");
+   
+
 }
 
-form.addEventListener("submit", executeForm);
-
-const getWeather = async () => { 
-    const weatherData = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${key}`);
-    .then(response = await response.json();
-        then (response => console.log(response + "is this working"))
-        // then(data => {
-            // const { main, name, sys, weather } = data
-            // const cityCard = document.getElementById("cityCard");
-            // const tempParagraph = document.createElement("p");
-            // const feelsLikeParagraph = document.createElement("p");
-            // const countryParagraph = document.createElement("p");
-            // const cityNameParagraph = document.createElement("p");
-            // tempParagraph.innerText = main.temp;
-            // feelsLikeParagraph.innerText = main.feels_like;
-            // countryParagraph.innerText = sys.country;
-            // cityNameParagraph.innerText = name;
-            // cityCard.append(tempParagraph);
-            // cityCard.append(feelsLikeParagraph);
-            // cityCard.append(countryParagraph);
-            // cityCard.append(cityNameParagraph);
-        // })
+const getWeather = (lat, lon) => { 
+    const weatherData = fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${key}`)
+    .then(response => response.json())
+        .then (data => {
+           
+            const { main, name, sys, weather } = data
+            const cityCard = document.getElementById("cityCard");
+            const weatherCondParagraph = document.createElement("p");
+            const tempParagraph = document.createElement("p");
+            const feelsLikeParagraph = document.createElement("p");
+            const countryParagraph = document.createElement("p");
+            const cityNameParagraph = document.createElement("p");
+            weatherCondParagraph.innerText = "Weather condition = " + weather.description;
+            tempParagraph.innerText = "Temperature = " + main.temp;
+            feelsLikeParagraph.innerText = "Feels like = " + main.feels_like;
+            countryParagraph.innerText = "Country = " + sys.country;
+            cityNameParagraph.innerText = "City location measurement = " + name;
+            cityCard.append(weatherCondParagraph);
+            cityCard.append(tempParagraph);
+            cityCard.append(feelsLikeParagraph);
+            cityCard.append(countryParagraph);
+            cityCard.append(cityNameParagraph);
+        })
         .catch(err => console.log(err))
-        
+   
 }
 
-const getCoordinates = (lat, lon) => {
+const getCoordinates = (cityName) => {
 
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${key}`)
         .then(response => response.json())
         .then(data => {
+            console.log(data)
             lat = data[0].lat;
             lon = data[0].lon;
-
-            console.log(data[0].lat + "this one is working");
-            console.log(data[0].lon + "I guess this one too");
+            console.log(lat, lon)
+            getWeather(lat, lon)
         })
         .catch(err => console.log(err))
-
 }
 
-// const waitOrder = async () => {
-//     const data = await getCoordinates();
-    
-//     return data;
-// }
-
+button.addEventListener("click", executeForm);
