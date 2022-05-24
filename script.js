177750e8c113d48e9e66fb8af85d453b8423562d
madalenaRio <madalena.rio@gmail,com>
2022-05-24 11:27:22 +0200

    trying to make both fetch work+ add await

--- script.js
+++ script.js
@@ -1,59 +1,72 @@
 
 import { ApiKey } from "./config.js";
-
-
-let lon = -9.1365919;
-let lat = 38.7077507;
+let lon;
+let lat;
 const key = ApiKey;
 
 const form = document.querySelector("form");
-const input = document.querySelector("input");
+const input = document.getElementById("input");
 const msg = document.getElementById("msg")
-const cityName = "Lisbon";
-const countryCode = "PT";
-
-const urlWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${key}`
-
-let inputVal = input.value;
 
 
-document.getElementById("form").addEventListener("submit", function(e){
-    if(!inputVal){
-        e.preventDefault();    //stop form from submitting  
-    }
-    getWeather() //do whatever and submit the form
+//const urlWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${key}`
 
-});
+const executeForm = (e) => {
+    // get the value from input textbox
+    e.preventDefault(); 
+    let inputVal = input.value;
+    // get coordinates from input
+    getCoordinates(inputVal);
+    waitOrder();
+    getWeather();    
+    console.log(inputVal);   
+}
 
-console.log ("is this weather thing working?");
-
+form.addEventListener("submit", executeForm);
 
 const getWeather = () => {
     
-    fetch(urlWeather)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${key}`)
         .then(response => response.json())
         //.then(response => console.log(response))
         .then(data => {
             const { main, name, sys, weather } = data;
             const cityCard = document.getElementById("cityCard");
             const tempParagraph = document.createElement("p");
+            const feelsLikeParagraph = document.createElement("p");
+            const countryParagraph = document.createElement("p");
+            const cityNameParagraph = document.createElement("p");
             tempParagraph.innerText = main.temp;
+            feelsLikeParagraph.innerText = main.feels_like;
+            countryParagraph.innerText = sys.country;
+            cityNameParagraph.innerText = name;
             cityCard.append(tempParagraph);
-
+            cityCard.append(feelsLikeParagraph);
+            cityCard.append(countryParagraph);
+            cityCard.append(cityNameParagraph);
         })
-        
         .catch(err => console.log(err))
 
 }
 
-const getCoordinates = () => {
+const getCoordinates = (cityName) => {
 
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${countryCode}&appid=${key}`)
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${key}`)
         .then(response => response.json())
-        //.then(response => console.log(response))
+        .then(data => {
+            lat = data[0].lat;
+            lon = data[0].lon;
+
+            console.log(data[0].lat);
+            console.log(data[0].lon);
+        })
         .catch(err => console.log(err))
 
 }
 
-getCoordinates()
+const waitOrder = async () => {
+    const response = await fetch()
+    const weatherData = await response.json();
+    return weatherData;
+}
 
